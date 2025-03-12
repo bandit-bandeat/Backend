9e5804ce1bcd055891371492ec759a5e45004833
py0o0 <pantom0425@naver.com>
2025-03-12 15:36:13 +0900

    first upload

---
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+    @Bean
+    public RouteLocator ecomRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // 개별 라우트 등록
+                // 서비스별 URL 별칭이 1개인 경우, n개인 경우도 존재
+                .route("auth",
+                        r -> r.path("/auth/**").uri("lb://auth"))
+                .build();
+    }
+}
---
+++ src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.gateway.config;
+
+import com.example.gateway.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 프론트엔드 URL
+        // configuration.addAllowedOrigin(frontIp);
+        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.addExposedHeader("access-token");
+
+        configuration.setAllowCredentials(true); // 쿠키 인증 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http.csrf(csrf -> csrf.disable());
+
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        http.securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
+
+        http.authorizeExchange(auth -> auth
+                .pathMatchers("/auth/**","/post/**"
+                ).permitAll()
+                .pathMatchers("/admin/**").hasRole("ROLE_ADMIN")
+                .anyExchange().authenticated());
+
+        http.addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION);
+
+        return http.build();
+    }
+}
