16cf5e4757660c00d52f959b8f37347aa93a05bf
py0o0 <pantom0425@naver.com>
2025-03-14 14:23:11 +0900

    kafka

--- src/main/java/com/example/user/UserApplication.java
+++ src/main/java/com/example/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
