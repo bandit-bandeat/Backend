6541d67bbf12dcb8a07170a8ed4cd198e5ed8c20
wqp99w <wqp99w@naver.com>
2025-03-17 13:40:14 +0900

    mp3 변환 기능 추가

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify, request, Blueprint, abort
+from flask import Flask, jsonify, request, Blueprint, abort, send_file, after_this_request
 from dotenv import load_dotenv
 import os
+import time
+import glob
 import eureka_client
 from models import db, is_change_able
+import music_changer
 
 import openai
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
@@ -26,29 +29,74 @@ app.config['KEY'] = os.getenv('OPEN_AI_KEY')
 openai.api_key = app.config['KEY']
 
 # GPT-4o Mini를 사용
-llm = OpenAI(model="gpt-4o-mini", temperature=0.7, api_key=app.config['KEY'])
+llm = OpenAI(model="gpt-3.5-turbo", temperature=0.7, api_key=app.config['KEY'])
 Settings.llm = llm
 
-documents = SimpleDirectoryReader('./data').load_data()
+# 코드 변환 쿼리 엔진
+documents = SimpleDirectoryReader('./data/code').load_data()
 index = GPTVectorStoreIndex(documents)
 query_engin = index.as_query_engine()
 
+# mp3 변환 쿼리 엔진
+documents_midi = SimpleDirectoryReader('./data/midi').load_data()
+index_midi = GPTVectorStoreIndex(documents)
+query_engin_midi = index.as_query_engine()
 
 # db 테이블 연결
 db.init_app(app)
 # 라우팅
 change = Blueprint('change', __name__, url_prefix='/change')
-
-
 @change.route('/music', methods=['POST'])
 def music_change():
     music_file = request.files['music_file']
     style = request.form['style']
+    email = request.form['email']
     print(type(music_file))
-
-    #print(music_changer.changer(music_file))
     print(style)
-    return "good"
+
+    if is_change_able(email):
+        midi_texts, base_name = music_changer.mp3_to_midi_text(music_file)
+        answers = ""
+        for i in range(4):
+            question = (f'대답은 한글로 해주고,'
+                        f'답변은 다른 말 없이, 미디 텍스트만 보여주고, 텍스트 길이는 입력 텍스트랑 동일하게 해줘'
+                        f'화음은 각각 한 음으로 표현해줘.\n{midi_texts[i]}\n '
+                        f'이 미디 텍스트를 {style}형식으로 바꿔줘\n'
+                        f'절대로 화음으로 표현하지 말고, 한 음으로 표현해야 해\n'
+                        f'Cmaj7 이런거 쓰지 말고 pretty_midi 라이브러리가 읽을 수 있게 출력해줘\n'
+                        f'Note: C2,Start Time: 28.659090909090907, End Time: 28.927272727272726, Velocity: 59 이 형식대로 출력해줘\n'
+                        f'그냥 C, D, 이렇게만 하면 안되고, C3 이렇게 적어야 해')
+            print(question)
+            response = query_engin_midi.query(question)
+            #print(response)s
+            answer = str(response)
+            answers += "\n" + answer
+        print("출력 잘 되남: ", answers)
+        mp3_path = music_changer.midi_text_to_mp3(answers, base_name)
+
+        # mp3 전송 후, 파일 삭제
+        @after_this_request
+        def remove_file(response):
+            try:
+                base_dir = os.path.dirname(mp3_path)
+                files_to_delete = glob.glob(os.path.join(base_dir, f"{base_name}*"))
+                for file_path in files_to_delete:
+                    try:
+                        os.remove(file_path)
+                        print(f"삭제됨: {file_path}")
+                    except Exception as e:
+                        print(f"파일 삭제 실패: {file_path}, 오류: {e}")
+            except Exception as e:
+                print(f'파일 삭제 실패: {e}')
+            return response
+        # 웹에서 실행하기 어려우면 요청 보내면 다운로드로 바로 되게 설정
+        #return send_file(mp3_path, as_attachment=True)
+        # 웹에서 mp3 파일 실행 가능하면 이걸로 사용
+
+        return send_file(mp3_path, mimetype="audio/mpeg")
+
+    return jsonify({"error": "하루 질문량을 초과했습니다."}), 400
+
 
 @change.route('/code', methods=['POST'])
 def code_change():
@@ -71,7 +119,7 @@ if __name__ == '__main__':
     print("유레카 연결")
     eureka_client.register_service()
     print("플라스크 실행")
-    #app.run(debug=False)
-    app.run(host='0.0.0.0', port=5000, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug = False)
+
 
 
--- music_changer.py
+++ music_changer.py
@@ -1,43 +1,161 @@
 import os
-
-from pydub import AudioSegment
-from pydub.utils import which
-import subprocess
-import tempfile
 import io
+import tempfile
+import subprocess
+from dotenv import load_dotenv
 
-# import tensorflow as tf
-# import magenta
-# from magenta.models.onsets_frames_transcription import onsets_frames_predictor
-# from magenta.models.onsets_frames_transcription import ONSETS_FRAMES_CONFIG
+from basic_pitch.inference import predict_and_save
+from basic_pitch import ICASSP_2022_MODEL_PATH
+from basic_pitch.inference import Model
+import pretty_midi
+from midi2audio import FluidSynth
+from pydub import AudioSegment
 
-# 시스템에서 FFmpeg과 FFprobe 자동 경로 설정
-ffmpeg_path = which("ffmpeg")
-ffprobe_path = which("ffprobe")
+import boto3
 
-# 경로가 없다면, FFmpeg을 설치할 위치를 확인하여 오류 처리
-if not ffmpeg_path or not ffprobe_path:
-    raise EnvironmentError("FFmpeg 또는 FFprobe가 시스템에 설치되지 않았습니다.")
+load_dotenv() # .env 파일 불러오기
 
-def changer( music_file ):
+def mp3_to_midi_text( music_file ):
     print(type(music_file))
-    # 업로드 된 파일을 mp3로 변환
-    music_data = music_file.read()
-    audio = AudioSegment.from_file(io.BytesIO(music_data))
-    # BytesIO 객체 생성 (메모리 내에서 바이트 스트림으로 저장)
-    wav_bytes = io.BytesIO()
-    audio.export(wav_bytes, format="wav")
-    # BytesIO 객체를 'asd'와 같은 변수로 사용할 수 있음
-    wav_bytes.seek(0)
 
-    # 임시 WAV 파일을 생성
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_wav:
-        tmp_wav.write(wav_bytes.getvalue())
-        wav_path = tmp_wav.name  # 임시 파일 경로
+    # mp3를 미디로 변환한 파일의 경로 받아오기
+    midi_path, base_name = mp3_to_midi(music_file)
+
+    print("미디 경로: ", midi_path)
+
+    midi_text = midi_to_text(midi_path, base_name)
+
+    return midi_text, base_name
 
 
-    # 임시 파일 삭제
-    os.remove(wav_path)
+def midi_text_to_mp3( midi_text, base_name ):
+    midi_path = text_to_midi(midi_text, base_name)
+    mp3_path = midi_to_mp3(midi_path)
+    return mp3_path
 
-    return "Asd"
 
+# 전자 피아노만 만드는 것 같긴 함 -> 기본모델이라 그런지는 모르겠음
+def mp3_to_midi(music_file):
+    # 음악 임시 저장하는 폴더 생성 -> temp_music으로 만들거고, 없으면 만들거임
+    temp_music_dir = './temp_music'
+    if not os.path.exists(temp_music_dir):
+        os.makedirs(temp_music_dir)
+
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3', dir='./temp_music') as temp_file:
+        music_file.save(temp_file)
+        music_path = temp_file.name
+
+    # mp3 변환에 사용하는 모델 선언 (basic_pitch 기본 모델 사용)
+    model = Model(ICASSP_2022_MODEL_PATH)
+
+    predict_and_save(
+        [music_path],
+        temp_music_dir,
+        True,
+        False,
+        False,
+        False,
+        model_or_model_path=model
+    )
+
+    # 저장된 미디 파일 경로 생성
+    base_name = os.path.splitext(os.path.basename(music_path))[0]
+    output_mid = f'temp_music/{base_name}_basic_pitch.mid'
+
+    return output_mid, base_name
+
+def midi_to_text(midi_path, base_name):
+    # 저장된 미디 파일 불러오기
+    midi_data = pretty_midi.PrettyMIDI(midi_path)
+
+    # 20초 간격으로 미디 텍스트를 저장 (지피티에 다 안들어감)
+    midi_texts = []
+
+    # 트랙별로 정보를 추출
+    for i, instrument in enumerate(midi_data.instruments) :
+        midi_text = []
+        midi_text.append(f"Instrument {i+1}: {instrument.program}")
+
+        end_time = 10
+        for note in instrument.notes:
+            midi_text.append(f"Note: {pretty_midi.note_number_to_name(note.pitch)},"
+                             f"Start Time: {note.start}, End Time: {note.end}, Velocity: {note.velocity}")
+            # 20초 간격으로 쪼개기
+            if note.end > end_time:
+                midi_texts.append(midi_text)
+                midi_text = []
+                end_time += 10
+                continue
+
+
+    temp_music_dir = './temp_music'
+
+    # midi_texts가 이중 배열일 경우, 각 리스트의 항목들을 이어서 리턴
+    return ['\n'.join(text) for text in midi_texts]
+
+def text_to_midi(midi_text, base_name):
+    # PrettyMIDI 객체 생성
+    midi_data = pretty_midi.PrettyMIDI()
+
+    instrument = pretty_midi.Instrument(program=4)
+
+    for line in midi_text.split("\n"):
+        if "Note:" in line:  # 노트 정보가 포함된 라인 찾기
+            print("지금 바꾸는 노트: ",line )
+            parts = line.split(",")
+            note_name = parts[0].split(":")[1].strip()  # 노트 이름 추출 (예: C4)
+            start_time = float(parts[1].split(":")[1].strip())  # 시작 시간
+            end_time = float(parts[2].split(":")[1].strip())  # 종료 시간
+            velocity = int(parts[3].split(":")[1].strip())  # 강도(velocity)
+
+            # 노트 추가 (PrettyMIDI는 pitch, start_time, end_time, velocity를 받음)
+            note = pretty_midi.Note(velocity=velocity, pitch=pretty_midi.note_name_to_number(note_name),
+                                    start=start_time, end=end_time)
+            instrument.notes.append(note)
+
+    midi_data.instruments.append(instrument)
+
+    midi_data.write(f'./temp_music/{base_name}.mid')
+    return f'./temp_music/{base_name}.mid'
+
+def midi_to_mp3(midi_path):
+    soundfont_path = download_soundfont_from_s3()
+    fs = FluidSynth(sound_font=soundfont_path)
+
+    result = subprocess.run(['fluidsynth', '--version'], capture_output=True, text=True)
+    wav_path = midi_path.replace(".mid", ".wav")  # 변환된 wav 파일 경로
+    output_mp3 = midi_path.replace(".mid", "out") # 출력할 mp3 파일 경로
+    output_mp3 += ".mp3"
+
+    fs.midi_to_audio(midi_path, wav_path) # 미디 파일을 wav 파일로 변환
+    audio = AudioSegment.from_wav(wav_path) # wav 파일 불러오기
+    audio.export(output_mp3, format="mp3")
+
+    return output_mp3
+
+def download_soundfont_from_s3():
+    bucket_name = os.getenv("S3_BUCKET_NAME")
+    s3_key = os.getenv("S3_KEY")
+    # './sound' 디렉토리에 저장할 경로 설정
+    local_file_path = './sound/soundfont.sf2'
+    # 디렉토리가 없다면 생성
+    os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+
+    # 파일이 이미 존재하면 다운로드하지 않음
+    if not os.path.exists(local_file_path):
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=os.getenv("S3_ACCESS_KEY"),  # 액세스 키
+            aws_secret_access_key=os.getenv("S3_SECRET_KEY"),  # 비밀 키
+            region_name=os.getenv("S3_REGION")  # 서울 리전
+        )
+
+        # S3에서 파일 다운로드
+        s3_client.download_file(bucket_name, s3_key, local_file_path)
+        print(f"파일이 다운로드되었습니다: {local_file_path}")
+    else:
+        print(f"파일이 이미 존재합니다: {local_file_path}")
+
+    # 로컬 파일 경로 반환
+    return local_file_path
